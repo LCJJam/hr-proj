72e985da9155f7514b75e583bfb833c04540cf0d
LeeChang_Mac <dlckd125@nate.com>
2024-10-13 16:13:22 +0900

    Initial commit

---
+++ src/main/java/com/hr/proj/HrProjApplication.java
@@ -0,0 +1,13 @@
+package com.hr.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hr/proj/HrProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
